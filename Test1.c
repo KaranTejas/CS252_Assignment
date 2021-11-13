fd74d149771812cf13f3e5f3b2c7718ac7c6f84e
KaranTejas <karantejas15@gmail.com>
2021-11-13 15:42:46 +0530

    Created Single Thread using Windows header file in  C

--- Test1.c
+++ Test1.c
@@ -1,8 +1,26 @@
 #include<stdio.h>
+#include<windows.h>
+
+DWORD WINAPI PrintParameters(int i){
+    printf("%d\n",i);
+    return 0;
+}
 
 int main(int argc, char *argv[])
 {
-    printf("%d,%s",argc,argv[0]);
+    DWORD ThreadId;
+    HANDLE ThreadHandle;
 
+    ThreadHandle = CreateThread(
+        NULL,
+        0,
+        PrintParameters,
+        atoi(argv[1]),
+        0,
+        &ThreadId
+    );
+    WaitForSingleObject(ThreadHandle,INFINITE);
+
+    printf("%d,%s",argc,argv[1]);
     return 0;
 }
