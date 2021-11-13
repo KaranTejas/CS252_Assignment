4e2c8bf2a47a021b333fac6b3764ee2cb357e17b
KaranTejas <karantejas15@gmail.com>
2021-11-13 20:27:05 +0530

    Added Devation about Mean and Median in Question 1

--- Question1.c
+++ Question1.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
+#include<math.h>
 #include<windows.h>
 #include<limits.h>
 
-float Average = 0;
+double Average = 0;
 int Maximum = INT_MIN;
 int Minimum = INT_MAX;
 int Median = 0;
-int StandardDeviationMean = 0;
-int StandardDeviationMedian = 0;
+double StandardDeviationMean = 0;
+double StandardDeviationMedian = 0;
 
 DWORD WINAPI AverageFunc(int *arr){
     int N = *arr;
@@ -17,10 +18,11 @@ DWORD WINAPI AverageFunc(int *arr){
         sum += *(arr + i);
     }
 
-    Average = ((float) sum) / N;
-    printf("Average = %f\n",Average);
+    Average = ((double) sum) / N;
+    printf("Average = %lf\n",Average);
     return 0;
 }
+
 DWORD WINAPI MaximumFunc(int *arr){
     int N = *arr;
 
@@ -28,9 +30,10 @@ DWORD WINAPI MaximumFunc(int *arr){
         Maximum = Maximum > *(arr + i) ? Maximum : *(arr + i);
     }
 
-    printf("Average = %d\n",Maximum);
+    printf("Maximum = %d\n",Maximum);
     return 0;
 }
+
 DWORD WINAPI MinimumFunc(int *arr){
     int N = *arr;
 
@@ -41,6 +44,7 @@ DWORD WINAPI MinimumFunc(int *arr){
     printf("Minimum = %d\n",Minimum);
     return 0;
 }
+
 DWORD WINAPI MedianFunc(int *arr){
     int N = *arr;
     int temp;
@@ -56,36 +60,43 @@ DWORD WINAPI MedianFunc(int *arr){
     }
 
     if(N % 2){
-        Median = (*(arr + (int)N/2) + *(arr + (int)N/2 + 1))/2;
+        Median = *(arr + (int)(N + 1)/2 );
     }
     else{
-        Median = *(arr + (int)(N + 1)/2 );
+        Median = (*(arr + (int)N/2) + *(arr + (int)N/2 + 1))/2;
     }
 
     printf("Median = %d\n",Median);
     return 0;
 }
+
 DWORD WINAPI StandardDeviationMeanFunc(int *arr){
     int N = *arr;
-    int Deviation = 0;
+    double Var = 0;
 
     for(int i = 1 ; i <= N ; i++){
-        Minimum = Minimum < *(arr + i) ? Minimum : *(arr + i);
+        Var += pow((Average - (double)*(arr + i)),2);
     }
 
+    StandardDeviationMean = sqrt(Var/N);
+    printf("Standard deviation about Mean = %.4lf\n",StandardDeviationMean);
     return 0;
 }
+
 DWORD WINAPI StandardDeviationMedianFunc(int *arr){
     int N = *arr;
-    int Deviation = 0;
+    double Var = 0;
 
     for(int i = 1 ; i <= N ; i++){
-        Minimum = Minimum < *(arr + i) ? Minimum : *(arr + i);
+        Var += pow((Median - (double)*(arr + i)),2);
     }
 
+    StandardDeviationMedian = sqrt(Var/N);
+    printf("Standard deviation about Median = %.4lf\n",StandardDeviationMedian);
     return 0;
 }
 
+// Storing the function addresses to use as callback functions while creating threads
 DWORD WINAPI (*PrintFunct[6])() = {
     AverageFunc,
     MaximumFunc,
@@ -100,6 +111,7 @@ int main(int argc, char *argv[])
     DWORD QuantityThreadId[4],DeviationThreadId[2];
     HANDLE QuantityThreadHandle[4],DeviationThreadHandle[2];
     int *arr = (int*) malloc(argc * sizeof(int));
+
     // arr(0) stores the size of the given array
     *arr = argc - 1;
     for(int i = 1 ; i < argc ; i++)
@@ -108,6 +120,7 @@ int main(int argc, char *argv[])
     }
 
     // First 4 callbacks functions are passed to 4 different threads
+
     for(int i = 0 ; i < 4 ; i++){
         QuantityThreadHandle[i] = CreateThread(
             NULL,
@@ -118,11 +131,30 @@ int main(int argc, char *argv[])
             &QuantityThreadId[i]
         );
     }
+
     WaitForMultipleObjects(4,QuantityThreadHandle,TRUE,INFINITE);
     for(int i = 0 ; i < 4 ; i++){
         CloseHandle(QuantityThreadHandle[i]);
     }
 
-    
+    // Last 2 functions calculate the Standard deviation using the previous functions
+    // Two separate threads are used
+
+    for(int i = 0 ; i < 2 ; i++){
+        QuantityThreadHandle[i] = CreateThread(
+            NULL,
+            0,
+            *PrintFunct[i + 4],
+            arr,
+            0,
+            &QuantityThreadId[i]
+        );
+    }
+
+    WaitForMultipleObjects(2,QuantityThreadHandle,TRUE,INFINITE);
+    for(int i = 0 ; i < 2 ; i++){
+        CloseHandle(QuantityThreadHandle[i]);
+    }
+
     return 0;
 }
